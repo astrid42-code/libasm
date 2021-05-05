8565d4d2d705cfd3f9cba4629df787e7ebe194d1
Astrid Gaultier <astridgaultier@gmail.com>
2021-05-05 10:48:19 +0200

    ft_strcmp3

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -5,22 +5,22 @@ ft_strcmp:
 		push rbp
 		mov rbp, rsp
 		xor rcx, rcx
+		xor rax, rax
 
 _loop:
-		cmp [rdi], byte 0
+		cmp [rdi + rcx], byte 0
 		je _end
 		cmp [rsi], byte 0
 		je _end
-		mov rcx, [rsi]
-		cmp [rdi], rcx
+		mov cx, [rsi + rcx]
+		cmp [rdi + rcx], cx
 		jne _end
-		inc rsi
-		inc rdi
+		inc rcx
 		jmp _loop
         
 _end:
-		mov rax, [rdi]
-		mov rbx, [rsi]
+		mov rax, [rdi + rcx]
+		mov rbx, [rsi + rcx]
 		sub rax, rbx
 		pop rbp
 		ret
