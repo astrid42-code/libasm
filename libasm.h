167a0e8a07da191eb95d6f42b9451995a189a586
asgaulti <asgaulti@student.42.fr>
2021-05-06 11:13:00 +0200

    strdup2

--- libasm.h
+++ libasm.h
@@ -30,6 +30,6 @@ void        ft_main_strdup(void);
 size_t      ft_strlen(const char *s);
 char        *ft_strcpy(char *dest, const char *src);
 int         ft_strcmp(const char *s1, const char *s2);
-char        *strdup(const char *s);
+char        *ft_strdup(const char *s);
 
 #endif
--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -1,25 +1,22 @@
 section .text
 		global ft_strdup
-		extern _malloc
-		extern _ft_strlen
-		extern _ft_strcpy
+		extern malloc
+		extern ft_strlen
+		extern ft_strcpy
 
 ft_strdup:
-        push rbp
-		mov rbp, rsp
-		call _ft_strlen ; rax = len de str (rsi)
-		mov r8, rsi ; copier rsi dans r8
-		inc rax ; +1 pour le \0
+		call ft_strlen ; rax = len de str (rsi)
+        inc rax ; +1 pour le \0
+		mov r8, rdi ; copier str dans r8 (ms pk rdi et pas rsi??)
 		mov rdi, rax ; pour envoyer la taille à malloc
-		call _malloc
+		call malloc
 		cmp rax, 0 ; si 0, malloc failed
 		jz _error_malloc
 		mov rdi, rax ; malloc le 1er param de cpy
 		mov rsi, r8 ; str remis dans rsi (2eme param de cpy)
-		call _ft_strcpy ; effectue la copie de rsi vers rdi
+		call ft_strcpy ; effectue la copie de rsi vers rdi
 		ret
 
 _error_malloc:
-		xor rax, rax
-		pop rbp
+		mov rax, -1
 		ret
