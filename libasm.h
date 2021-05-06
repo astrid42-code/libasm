c4d684c1c74d4c645bffef3b4bffb6d8dd902fad
asgaulti <asgaulti@student.42.fr>
2021-05-06 17:30:17 +0200

    strdup3

--- libasm.h
+++ libasm.h
@@ -25,11 +25,13 @@ void		ft_main_strlen(void);
 void		ft_main_strcpy(void);
 void        ft_main_strcmp(void);
 void        ft_main_strdup(void);
+void        ft_main_write(void);
 
 //fcts asm
 size_t      ft_strlen(const char *s);
 char        *ft_strcpy(char *dest, const char *src);
 int         ft_strcmp(const char *s1, const char *s2);
 char        *ft_strdup(const char *s);
+ssize_t     ft_write(int fd, const void *buf, size_t count); 
 
 #endif
--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,9 +5,11 @@ section .text
 		extern ft_strcpy
 
 ft_strdup:
+		push rbp
+		mov rbp, rsp
+		mov r8, rdi ; copier str dans r8 (ms pk rdi et pas rsi??)
 		call ft_strlen ; rax = len de str (rsi)
         inc rax ; +1 pour le \0
-		mov r8, rdi ; copier str dans r8 (ms pk rdi et pas rsi??)
 		mov rdi, rax ; pour envoyer la taille à malloc
 		call malloc
 		cmp rax, 0 ; si 0, malloc failed
@@ -15,8 +17,10 @@ ft_strdup:
 		mov rdi, rax ; malloc le 1er param de cpy
 		mov rsi, r8 ; str remis dans rsi (2eme param de cpy)
 		call ft_strcpy ; effectue la copie de rsi vers rdi
+		pop rbp
 		ret
 
 _error_malloc:
 		mov rax, -1
+		pop rbp
 		ret
